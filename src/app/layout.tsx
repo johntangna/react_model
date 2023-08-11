6b9b387e90baf6322111e4bb39dfe4690bc75d71
williemwang <1092473788@qq.com>
2023-08-11 15:23:12 +0800

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,105 @@
+import * as React from 'react';
+import Link from 'next/link';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import AppIcon from "./icon/index"
+import ThemeRegistry from '@/components/ThemeRegistry/ThemeRegistry';
+
+export const metadata = {
+  title: '欧美斯商城-专业美妆护肤，只为最美的你',
+  description: 'onmouse,欧美斯，美妆',
+};
+
+const DRAWER_WIDTH = 240;
+
+const LINKS = [
+  { text: 'Home', href: '/', icon: AppIcon.HomeIcon },
+  { text: 'Starred', href: '/starred', icon: AppIcon.StarIcon },
+  { text: 'Tasks', href: '/tasks', icon: AppIcon.ChecklistIcon },
+];
+
+const PLACEHOLDER_LINKS = [
+  { text: 'Settings', icon: AppIcon.SettingsIcon },
+  { text: 'Support', icon: AppIcon.SupportIcon },
+  { text: 'Logout', icon: AppIcon.LogoutIcon },
+];
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <ThemeRegistry>
+          <AppBar position="fixed" sx={{ zIndex: 2000 }}>
+            <Toolbar sx={{ backgroundColor: 'background.paper' }}>
+              <AppIcon.DashboardIcon sx={{ color: '#444', mr: 2, transform: 'translateY(-2px)' }} />
+              <Typography variant="h6" noWrap component="div" color="black">
+                美妆护肤
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Drawer
+            sx={{
+              width: DRAWER_WIDTH,
+              flexShrink: 0,
+              '& .MuiDrawer-paper': {
+                width: DRAWER_WIDTH,
+                boxSizing: 'border-box',
+                top: ['48px', '56px', '64px'],
+                height: 'auto',
+                bottom: 0,
+              },
+            }}
+            variant="permanent"
+            anchor="left"
+          >
+            <Divider />
+            <List>
+              {LINKS.map(({ text, href, icon: Icon }) => (
+                <ListItem key={href} disablePadding>
+                  <ListItemButton component={Link} href={href}>
+                    <AppIcon.ListItemIcon>
+                      <Icon />
+                    </AppIcon.ListItemIcon>
+                    <AppIcon.ListItemText primary={text} />
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+            <Divider sx={{ mt: 'auto' }} />
+            <List>
+              {PLACEHOLDER_LINKS.map(({ text, icon: Icon }) => (
+                <ListItem key={text} disablePadding>
+                  <ListItemButton>
+                    <AppIcon.ListItemIcon>
+                      <Icon />
+                    </AppIcon.ListItemIcon>
+                    <AppIcon.ListItemText primary={text} />
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+          </Drawer>
+          <Box
+            component="main"
+            sx={{
+              flexGrow: 1,
+              bgcolor: 'background.default',
+              ml: `${DRAWER_WIDTH}px`,
+              mt: ['48px', '56px', '64px'],
+              p: 3,
+            }}
+          >
+            {children}
+          </Box>
+        </ThemeRegistry>
+      </body>
+    </html>
+  );
+}
